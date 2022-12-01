fd33f2bfeabf359079e2b0ae6d3e875e8e8fee39
Iva <ivahrkac123@gmail.com>
2022-12-01 14:33:16 +0100

    register backend and frontend

---
+++ tab-tracker/src/views/Register.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>
+        Register
+    </h1>
+    <input 
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email" />
+    <br>
+    <input 
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password" />
+  </div>
+  <br>
+  <button @click="register"> 
+  Register 
+  </button>
+</template>
+
+<script>
+import AuthenicationService from '@/services/AuthenticationService'
+export default ({
+    data() {
+        return{
+            email:'',
+            password:''
+        }
+    },
+    methods: {
+        async register(){
+            const response = await AuthenicationService.register({
+                email:this.email,
+                password:this.password
+            })
+           console.log(response.data)
+        }
+    }
+})
+</script>
+
+<style>
+</style>
